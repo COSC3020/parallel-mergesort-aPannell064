79f68f8f6d34767f9e210f3a7f4660144c2c67d6
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-05-01 20:49:35 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -5,7 +5,7 @@ eval(fs.readFileSync('code.js')+'');
 
 const testSort = jsc.asyncProperty("array nat", function (arr, done) {
     const a1 = JSON.parse(arr);
-    const expected = JSON.parse(arr).sort(function(a, b) { return a - b;};
+    const expected = JSON.parse(arr).sort(function(a, b) { return a - b;});
 
     pmsMR(a1, function (result) {
         const pass = JSON.stringify(result) === JSON.stringify(expected);
